2fe912b0bba4221fe15d4af38118c14b1350e787
ilaydaguzel <ilaydaguzel257@gmail.com>
2026-02-20 16:26:18 +0300

    Kayıt ve giriş sistemi oluşturuldu, hesap oluşturma sırasında hesap türü seçimi eklendi.

--- app.py
+++ app.py
@@ -1,7 +1,54 @@
-from flask import Flask
+from flask import Flask,render_template,redirect,url_for,request,session
 app=Flask(__name__)
+app.secret_key="secret"
+users=[]
+
 @app.route('/')
 def home():
-    return "Uygulama calisiyor"
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        role = request.form['role']
+        users.append({'username':username,'password':password,'role':role})
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        for user in users:
+            if user['username'] == username and user['password'] == password:
+                session['username']=username and user["password"]==password
+                session['role']=user['role']
+                if user['role']=='teacher':
+                    return redirect(url_for('teacher_panel'))
+                else:
+                    return redirect(url_for('student_panel'))
+                return "Hatalı giriş!"
+    return render_template("login.html")
+
+@app.route('/teacher')
+def teacher_panel():
+    if session.get('role')!='teacher':
+        return "Yetki yok"
+    return "Öğretmen Paneli"
+
+@app.route('/student')
+def student_panel():
+    if session.get('role') != 'student':
+        return "Yetki yok"
+    return "Öğrenci Paneli"
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
 if __name__=="__main__":
     app.run(debug=True)
